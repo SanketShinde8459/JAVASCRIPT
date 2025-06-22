b533a38a5bfb12ac8dce99d31f2b724f6a05d49d
SanketShinde8459 <sanketshinde8459@gmail.com>
2025-06-23 00:18:24 +0530

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//***************LET VAR SCOPE************** *
 // function cal(a,b){
 //   let sum = a + b;
 //   console.log(sum);
@@ -25,6 +25,8 @@
 //     var sum = 5;
 // }
 // console.log(sum); // possible
+
+//**********OBJECTS************************** */
 // let students = {
 //     name:"sanket",
 //     age:20,
@@ -43,13 +45,63 @@
 //     }
 // };
 
+// let students = [
+//     {
+//         name: "sanket",
+//         age: 20,
+//         qualification: "bsc cs"
+//     },
+//     {
+//         name: "pranav",
+//         age: 20,
+//         qualification: "bsc cs"
+//     }
+// ]
 
-// 
+// ***********FUNCTIONS***************
 
-function cal(a,b){
-    let sum = a + b;
-    return sum;
-}
-let result = cal(2,5);
-console.log(result); // 
+// function cal(a,b){
+//     let sum = a + b;
+//     return sum;
+// }
+// let result2 = cal(2,5);
+// console.log(result2); // 
 
+// let sum = function(){
+//     let a = 9;
+//     let b = 5;
+//     return a + b;
+// }
+// let result = sum();
+// console.log(result); // 7
+// console.log(sum); // 14
+
+// let sum = (a,b) => {
+//     return a + b;
+// };
+// let result = sum(999,1);
+// console.log(result); // 7
+
+// let sum2 = (a,b) => a + b;
+// let result2 = sum2(999,1);
+// console.log(result2); // 7
+
+// let ex = {
+//     c: 10,
+//     d: 20,
+//     sum: function() {
+//         return this.c + this.d;
+//     }
+// }
+// let result = ex.sum();
+// console.log(result); // 30
+
+// let ex1 = {
+//     c: 10,
+//     d: 20,
+//     sum: () => {
+//         return this.c + this.d;
+//     }
+// }
+// let result2 = ex1.sum();
+// console.log(result2); // NAN
